ca4e454c570b15a528f14f2eaf3d259cd6a00f99
Diego Belay <diego.belay@gmail.com>
2024-02-02 13:44:07 -0300

    Debug touch position

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -12,7 +12,19 @@ public class BallHandler : MonoBehaviour
 
     void Update()
     {
-        Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        UnityEngine.InputSystem.Controls.TouchControl touch = Touchscreen.current.primaryTouch;
+
+        if (!touch.press.isPressed)
+        {
+            return;
+        }
+
+        Vector2 touchPos = touch.position.ReadValue();
 
         Debug.Log($"[BallHandler] touchPos: {touchPos}");
     }
