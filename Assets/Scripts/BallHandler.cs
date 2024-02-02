f0fb906685a23d59d263b449701042189091132c
Diego Belay <diego.belay@gmail.com>
2024-02-02 14:03:40 -0300

    Check touch pos infinity

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -28,6 +28,13 @@ public class BallHandler : MonoBehaviour
 
         Vector2 touchScreenPos = touch.position.ReadValue();
 
+        if (touchScreenPos.x.Equals(float.PositiveInfinity) ||
+            touchScreenPos.y.Equals(float.PositiveInfinity))
+        {
+            //Debug.Log("false touch positive INFINITY");
+            return;
+        }
+
         Vector3 touchWorldPos = _mainCamera.ScreenToWorldPoint(touchScreenPos);
 
         Debug.Log($"[BallHandler] touchWorldPos: {touchWorldPos}, touchScreenPos: {touchScreenPos}");
