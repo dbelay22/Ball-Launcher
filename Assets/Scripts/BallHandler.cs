302b839c6c1ad4facba26e0921ff48b7787de692
Diego Belay <diego.belay@gmail.com>
2024-02-10 14:34:35 -0300

    Refactor ball handler

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,38 +1,104 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _currentBallRB;
 
+    bool _serializedFieldsError = false;
+
     Camera _mainCamera;
+
+    TouchControl _touch;
+
+    bool _isDragging;
+
+    bool _launchedBall;
+
+    void Awake()
+    {
+        CheckSerializedFields();
+    }
+
+    void CheckSerializedFields() 
+    {
+        _serializedFieldsError = false;
+
+        if (_currentBallRB == null)
+        {
+            Debug.LogError("BallHandler] _currentBallRB is invalid");
+            _serializedFieldsError = true;
+        }
+    }
+
+    bool AnyError()
+    {
+        return _serializedFieldsError;
+    }
 
     void Start()
     {
         _mainCamera = Camera.main;
+
+        _touch = Touchscreen.current.primaryTouch;
+
+        _isDragging = false;
+
+        _launchedBall = false;
     }
 
     void Update()
     {
+        if (AnyError() == true) { return; }
+
+        if (_launchedBall == true) { return; }
+
         ProcessInput();
     }
 
-    private void ProcessInput()
+    void ProcessInput()
     {
-        UnityEngine.InputSystem.Controls.TouchControl touch = Touchscreen.current.primaryTouch;
+        bool touchIsPressing = touchIsPressed();
 
-        bool isPressed = touch.press.isPressed;
-
-        SetKinematic(isPressed);
-
-        if (!isPressed)
+        if (touchIsPressing)
         {
-            return;
-        }       
+            OnTouchPressing();
+        }
+        else
+        {
+            OnTouchRelease();
+        }
+    }
 
-        Vector2 touchScreenPos = touch.position.ReadValue();
+    void OnTouchPressing()
+    {
+        _isDragging = true;
+
+        DragBall();
+    }
+
+    void OnTouchRelease()
+    {
+        // finger out
+        if (_isDragging)
+        {
+            _isDragging = false;
+
+            LaunchBall();
+        }
+    }
+
+    void DragBall()
+    {
+        // disable physics for the ball's RB
+        SetKinematic(true);
+
+        // read touch position
+        Vector2 touchScreenPos = _touch.position.ReadValue();
 
         if (touchScreenPos.x.Equals(float.PositiveInfinity) ||
             touchScreenPos.y.Equals(float.PositiveInfinity))
@@ -41,15 +107,30 @@ public class BallHandler : MonoBehaviour
             return;
         }
 
+        // convert screen -> world position
         Vector3 touchWorldPos = _mainCamera.ScreenToWorldPoint(touchScreenPos);
 
         //Debug.Log($"[BallHandler] touchWorldPos: {touchWorldPos}, touchScreenPos: {touchScreenPos}");
 
+        // update ball's rigid body position
         _currentBallRB.position = touchWorldPos;
+    }    
+
+    void LaunchBall()
+    {
+        // re-enable physics for the ball's RB
+        SetKinematic(false);
+
+        _launchedBall = true;
     }
 
     void SetKinematic(bool kinematic)
     {
         _currentBallRB.isKinematic = kinematic;
+    }
+
+    bool touchIsPressed()
+    {
+        return _touch.press.isPressed;
     }
 }
