2660f471a4743f6f622b5bea03b5353290682e92
Diego Belay <diego.belay@gmail.com>
2024-02-15 12:20:25 -0300

    Add YLogger log level

--- Assets/Scripts/Yxp/Debug/YLogger.cs
+++ Assets/Scripts/Yxp/Debug/YLogger.cs
@@ -8,15 +8,20 @@ namespace Yxp.Debug
 
         static ILogger _currentLogger;
 
+        static YLogLevel _logLevel;
+
         static YLogger()
         {
-            _currentLogger = new UnityLogger();
-            _enabled = true;
+            _currentLogger = new UnityLogger();            
+            _enabled = true;            
+            _logLevel = YLogLevel.Debug;
         }
 
         public static void Debug(object message, object sender = null)
         {
             if (!_enabled) return;
+
+            if (_logLevel > YLogLevel.Debug) return;
 
             _currentLogger.Debug(message, sender);
         }
@@ -25,12 +30,16 @@ namespace Yxp.Debug
         {
             if (!_enabled) return;
 
+            if (_logLevel > YLogLevel.Warning) return;
+
             _currentLogger.Warning(message, sender);
         }
 
         public static void Error(object message, object sender = null)
         {
             if (!_enabled) return;
+
+            if (_logLevel > YLogLevel.Error) return;
 
             _currentLogger.Error(message, sender);
         }
@@ -39,6 +48,18 @@ namespace Yxp.Debug
         {
             _enabled = enabled;
         }
+
+        public static void SetLogLevel(YLogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+    }
+
+    public enum YLogLevel
+    { 
+        Debug = 0,
+        Warning = 10,
+        Error = 20
     }
 
 }
